3d82e839b4e8905d651adc41a95f4ba103a6a51d
gajop <gajo01@gmail.com>
2013-09-24 02:55:00 +0200

    basic restful URLs

--- webservice/views.py
+++ webservice/views.py
@@ -1,1 +1,12 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+@csrf_exempt
+def submit_answer(request, pk):
+    if request.method == 'GET':
+        pass
+
+    elif request.method == 'PUT':
+        pass
+
+    else:
+        return HttpResponse(status=204)
