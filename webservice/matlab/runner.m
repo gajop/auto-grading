b8c5c4f024b031131b3232ce65da76bfa33e645f
Gajo Petrovic <gajop@uns.ac.rs>
2013-12-01 08:47:27 +0100

    basic error checking

--- webservice/matlab/runner.m
+++ webservice/matlab/runner.m
@@ -12,9 +12,17 @@ rmpath(correctPath);
 
 addpath(submittedPath);
 submittedReturns = {};
+errors = {};
 for i = 1:length(ts)
     t = ts{i};
-    submittedReturns{i} = t();
+    try
+        submittedReturns{i} = t();
+        errors{i} = 0;
+    catch
+        submittedReturns{i} = 0; % exact value doesn't matter, should never be checked
+        msg = lasterror.message;
+        errors{i} = msg;
+    end
 end
 rmpath(submittedPath);
 disp("|||STARTPRINT|||")
@@ -22,9 +30,10 @@ disp("|||STARTPRINT|||")
 outputString = "";
 successfulAmount = 0;
 for i = 1:length(ts)
-    correctReturn = correctReturns{i}
-    submittedReturn = submittedReturns{i}
-    if correctReturn == submittedReturn
+    correctReturn = correctReturns{i};
+    if errors{i} ~= 0
+        outputString = [outputString "Test " int2str(i) " netačan. " errors{i} " \n"];
+    elseif correctReturn == submittedReturns{i}
         outputString = [outputString "Test " int2str(i) " tačan.\n"];
         successfulAmount = successfulAmount + 1;
     else
