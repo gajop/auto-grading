63b2e65b928b6d8700828475e7a83adc22508ef5
Gajo Petrovic <gajop@uns.ac.rs>
2013-12-03 18:23:38 +0100

    changed docker to use the git version, fixed settings_production

--- automatic_grading_ftn/settings_production.py
+++ automatic_grading_ftn/settings_production.py
@@ -8,6 +8,7 @@ DATABASES['default']['name'] = '/home/grading/database.db'
 
 ALLOWED_HOSTS = ['*']
 
-MEDIA_ROOT = '/home/gajop/grading/uploads'
-STATIC_ROOT = '/home/gajop/grading/webservice/static'
+MEDIA_ROOT = '/home/grading/uploads'
+STATIC_ROOT = '/home/grading/webservice/static/'
+STATICFILES_DIRS = ('/home/grading/webservice/static',)
 LOGGING['handlers']['logfile']['filename'] = '/var/log/django/grading.log'
